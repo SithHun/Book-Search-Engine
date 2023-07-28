0b892718a6457edb43910da875cd8c1b1782c947
Seth Sun <sithsun@gmail.com>
2023-07-28 12:29:55 -0400

    Imported localStorage to savedBooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,6 +9,7 @@ import {
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
+import { removeBookId } from '../utils/localStorage';
 import Auth from '../utils/auth';
 
 const SavedBooks = () => {
