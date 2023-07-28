054433dc2bb7360a93a591572bce899b9a57b3cd
Seth Sun <sithsun@gmail.com>
2023-07-28 15:38:32 -0400

    Debugging token error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,35 +1,35 @@
-const { AuthenticationError } = require('apollo-server-express');
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'mysecretssshhhhhhh';
 const expiration = '2h';
 
 module.exports = {
-  authMiddleware: context => {
-    const { req } = context
-    let token
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // We split the token string into an array and return actual token
     if (req.headers.authorization) {
-      token = req.headers.authorization.split(' ').pop().trim();
+      token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      throw new AuthenticationError('You have no token!');
+      return req;
     }
 
-    // verify token and get user data out of it
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
 
+    // return the request object so it can be passed to the resolver as `context`
+    return req;
+  },
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
