f0d581f8fb957c98cebe38f930b4586a3a6a19a5
Seth Sun <sithsun@gmail.com>
2023-07-28 11:51:44 -0400

    Trying to debug book schema issue

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,34 +2,34 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type Book {
-    bookId: ID
     authors: [String]
-    description: String
-    title: String
+    description: String!
+    bookId: String!
     image: String
     link: String
+    title: String!
   }
 
   type User {
     _id: ID
-    username: String
-    email: String
+    username: String!
+    email: String!
     savedBooks: [Book]
     bookCount: Int
   }
 
   type Auth {
-    token: ID
+    token: ID!
     user: User
   }
 
   input BookInput {
-    bookId: ID
     authors: [String]
-    description: String
-    title: String
+    description: String!
+    bookId: String!
     image: String
     link: String
+    title: String!
   }
 
   type Query {
